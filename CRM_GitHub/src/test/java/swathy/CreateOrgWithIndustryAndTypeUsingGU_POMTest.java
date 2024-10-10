8d8befcf3835c9ccc4f6549f67ce32e0bcb824a5
USER <USER@LAPTOP-A7S14MCN>
2024-10-10 20:16:54 +0530

    fixed

--- CRM_GitHub/src/test/java/swathy/CreateOrgWithIndustryAndTypeUsingGU_POMTest.java
+++ CRM_GitHub/src/test/java/swathy/CreateOrgWithIndustryAndTypeUsingGU_POMTest.java
@@ -118,4 +118,3 @@ public class CreateOrgWithIndustryAndTypeUsingGU_POMTest {
 	}
 
 }
-}
--- CRM_GitHub/src/test/java/swathy/ToLearnFork.java
+++ CRM_GitHub/src/test/java/swathy/ToLearnFork.java
@@ -1,7 +1,10 @@
 package swathy;
 
+import org.testng.annotations.Test;
+
 public class ToLearnFork {
 
+	@Test
 	public void demoStart() {
 		System.out.println("am starting my fork");
 	}
